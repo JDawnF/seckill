49b42fe0a55083131332535118d592cae5137f6f
JDawnF <335825732@qq.com>
2019-10-05 22:37:02 +0800

    秒杀项目

---
+++ src/main/java/com/baichen/seckill/SecKillApplication.java
@@ -0,0 +1,16 @@
+package com.baichen.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @Date: 2019-10-05 22:32
+ * @Author: baichen
+ * @Description
+ */
+@EnableAutoConfiguration
+public class SecKillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecKillApplication.class, args);
+    }
+}
