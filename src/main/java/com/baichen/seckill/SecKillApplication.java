9ad104b9ab56e792fbd3384901ecfdef0ba9f3ac
JDawnF <335825732@qq.com>
2019-10-05 23:25:06 +0800

    逆向工程生成pojo和mapper

--- src/main/java/com/baichen/seckill/SecKillApplication.java
+++ src/main/java/com/baichen/seckill/SecKillApplication.java
@@ -1,14 +1,19 @@
 package com.baichen.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Date: 2019-10-05 22:32
  * @Author: baichen
  * @Description
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.baichen.seckill.pojo"})
+@RestController
+@MapperScan("com.baichen.seckill.dao")
 public class SecKillApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecKillApplication.class, args);
